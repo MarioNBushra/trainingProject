3f5fe79d557b6df59055a4a2db695362ede1fac2
MarioNBushra <mariobushra@outlook.com>
2022-02-03 10:59:16 +0200

    db

--- db/mongoose.js
+++ db/mongoose.js
@@ -1,7 +1,7 @@
 const mongoose = require("mongoose");
 
 try {
-  let dbLink = "mongodb://127.0.0.1:27017/abannoub";
+  let dbLink = process.env.DB_URL || "mongodb://127.0.0.1:27017/abannoub";
   mongoose.connect(dbLink, {
     useNewUrlParser: true,
     useUnifiedTopology: true,
