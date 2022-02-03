81576ede90168ca07417dc83c67ca3276bc32517
MarioNBushra <mariobushra@outlook.com>
2022-02-03 11:40:01 +0200

    login

--- controllers/userController.js
+++ controllers/userController.js
@@ -9,3 +9,17 @@ exports.signup = async (req, res) => {
     res.status(400).send({ msg: error.message });
   }
 };
+
+exports.signin = async (req, res) => {
+  try {
+    const user = await UserModel.findByCredentials(
+      req.body.email,
+      req.body.password
+    );
+
+    const token = await user.generateAuthToken();
+    res.send({ user, token });
+  } catch (error) {
+    res.status(400).send({ msg: error.message });
+  }
+};
